c31c41ea0174f882f4e16b57c29edc9f05e019e8
Maarten Dhondt <maarten.dhondt@skynet.be>
2016-11-27 16:26:12 +0100

    rename module

--- domain/src/main/java/be/scoutsronse/wafelbak/entity/AbstractEntity.java
+++ domain/src/main/java/be/scoutsronse/wafelbak/entity/AbstractEntity.java
@@ -3,9 +3,10 @@ package be.scoutsronse.wafelbak.entity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @MappedSuperclass
-abstract class AbstractEntity {
+abstract class AbstractEntity implements Serializable {
     @Id
     @GenericGenerator(name = "sequenceGenerator", strategy = "enhanced-sequence", parameters = {
             @org.hibernate.annotations.Parameter(name = "optimizer", value = "pooled-lo"),
