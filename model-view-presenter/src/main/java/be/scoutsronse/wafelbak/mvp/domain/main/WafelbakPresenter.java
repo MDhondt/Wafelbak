297c740ceee8ac2c9b03c09a0fa84fd9d427bc95
Maarten Dhondt <maarten.dhondt@skynet.be>
2018-01-11 23:24:10 +0100

    test mapview

--- model-view-presenter/src/main/java/be/scoutsronse/wafelbak/mvp/domain/main/WafelbakPresenter.java
+++ model-view-presenter/src/main/java/be/scoutsronse/wafelbak/mvp/domain/main/WafelbakPresenter.java
@@ -1,6 +1,5 @@
 package be.scoutsronse.wafelbak.mvp.domain.main;
 
-import be.scoutsronse.wafelbak.domain.entity.Street;
 import be.scoutsronse.wafelbak.mvp.Presenter;
 import be.scoutsronse.wafelbak.repository.api.StreetRepository;
 import org.springframework.stereotype.Component;
@@ -12,13 +11,4 @@ public class WafelbakPresenter extends Presenter<WafelbakModel, WafelbakView> {
 
     @Inject
     private StreetRepository streetRepository;
-
-    public void persist(Street street) {
-        streetRepository.save(street);
-    }
-
-    public void test() {
-        System.out.println(streetRepository.findAll());
-        System.out.println("testing");
-    }
 }
--- model-view-presenter/src/main/java/be/scoutsronse/wafelbak/mvp/domain/main/WafelbakView.java
+++ model-view-presenter/src/main/java/be/scoutsronse/wafelbak/mvp/domain/main/WafelbakView.java
@@ -3,15 +3,22 @@ package be.scoutsronse.wafelbak.mvp.domain.main;
 import be.scoutsronse.wafelbak.mvp.View;
 import com.sothawo.mapjfx.Coordinate;
 import com.sothawo.mapjfx.MapView;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import static java.lang.Double.MAX_VALUE;
 
 public class WafelbakView extends View<WafelbakPresenter> {
 
     private Stage primaryStage;
-    private Button button;
     private MapView mapView;
+    private BorderPane borderPane;
 
     public WafelbakView(WafelbakPresenter presenter) {
         super(presenter);
@@ -27,25 +34,41 @@ public class WafelbakView extends View<WafelbakPresenter> {
     }
 
     private void populateStage() {
-        button = new Button("test");
-        button.setOnAction(event -> presenter().test());
+        borderPane = new BorderPane();
+        borderPane.setCenter(createMapView());
+        borderPane.setLeft(createLeftTools());
 
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
+//        primaryStage.setMaximized(true);
+    }
+
+    private Node createLeftTools() {
+        Accordion leftTools = new Accordion();
+        VBox boxForButton = new VBox(20);
+        boxForButton.getChildren().add( new Button("click me"));
+        TitledPane titledPane = new TitledPane("test title pane",boxForButton);
+        leftTools.getPanes().add(titledPane);
+        leftTools.setExpandedPane(titledPane);
+        leftTools.setMinWidth(250);
+
+        return leftTools;
+    }
+
+    private MapView createMapView() {
         mapView = new MapView();
         mapView.setCustomMapviewCssURL(getClass().getResource("/mapview.css"));
         mapView.initializedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                afterMapIsInitialized();
+                mapView.setZoom(14);
+                mapView.setCenter(new Coordinate(50.749077, 3.599492));
             }
         });
         mapView.initialize();
+        mapView.setMaxSize(MAX_VALUE, MAX_VALUE);
+        mapView.setMinSize(750,750);
 
-        Scene scene = new Scene(mapView);
-        primaryStage.setScene(scene);
-//        primaryStage.setMaximized(true);
-    }
-
-    private void afterMapIsInitialized() {
-        mapView.setZoom(14);
-        mapView.setCenter(new Coordinate(50.749077, 3.599492));
+        return mapView;
     }
 }
